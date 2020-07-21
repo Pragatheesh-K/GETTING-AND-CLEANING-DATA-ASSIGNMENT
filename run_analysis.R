8159b5444d1738bcf7345bc58d41b477d4a1a78b
Pragatheesh K <63781729+Pragatheesh-K@users.noreply.github.com>
2020-07-21 22:10:30 +0530

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+library(dplyr)
+
+feat<- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = feat$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = feat$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+MData <- cbind(Subject, Y, X)
+
+TData <- MData %>% select(subject, code, contains("mean"), contains("std"))
+
+TData$code <- activities[TData$code, 2]
+
+names(TData)[2] = "activity"
+names(TData)<-gsub("Acc", "Accelerometer", names(TData))
+names(TData)<-gsub("Gyro", "Gyroscope", names(TData))
+names(TData)<-gsub("BodyBody", "Body", names(TData))
+names(TData)<-gsub("Mag", "Magnitude", names(TData))
+names(TData)<-gsub("^t", "Time", names(TData))
+names(TData)<-gsub("^f", "Frequency", names(TData))
+names(TData)<-gsub("tBody", "TimeBody", names(TData))
+names(TData)<-gsub("-mean()", "Mean", names(TData), ignore.case = TRUE)
+names(TData)<-gsub("-std()", "STD", names(TData), ignore.case = TRUE)
+names(TData)<-gsub("-freq()", "Frequency", names(TData), ignore.case = TRUE)
+names(TData)<-gsub("angle", "Angle", names(TData))
+names(TData)<-gsub("gravity", "Gravity", names(TData))
+
+FData <- TData %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(FData, "FinalData.txt", row.name=FALSE)
